2291e689f6456ae0c67dca8e91843c06657060ac
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-09 19:16:58 +1000

    db done.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+  
+INSERT INTO burgers (name) VALUES ('Burger Chicken');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, devour) VALUES ('Mince Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Lamb Burger', true);
