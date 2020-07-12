dc346170a60ae4815fbba90492349e2943ea46ae
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-13 08:44:30 +1000

    colors package added for terminal text. Server file all done and commented.

--- server.js
+++ server.js
@@ -1,30 +1,22 @@
+//Dependencies
 var express = require("express"); 
-
 var PORT = process.env.PORT || 8080;
-
 var app = express();
+var colors = require('colors'); 
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Set Handlebars.
+//Set up handlebars.
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
+//Getting routes and using them
 var routes = require("./controllers/burgers_controller.js");
-
 app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
+//Start the server.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log(colors.red.underline("Server listening on: http://localhost:" + PORT));
 });
 
