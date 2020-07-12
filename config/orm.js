d4a7db2ebc45cafa7525401ad2892c42437bfd21
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-13 08:19:53 +1000

    orm done. comments done.

---
+++ config/orm.js
@@ -0,0 +1,82 @@
+//Require connection js file from this folder. 
+//Define  as connection
+var connection = require("../config/connection.js");
+//This function is a for loop that returns an array of question marks and converts it to a string. 
+//This is for querying the db.  
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+//This function converts objects into sql syntax. 
+//It takes the key value pair format and makes it readable for the sql server. 
+function objToSql(ob) {
+  //empty array to hold what gets pushed. 
+  var arr = [];
+
+//Use a for loop again to loop through the keys.
+//Then push the results as a string to the empty array. 
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
+//This is the object for all of the SQL functions that will be needed. 
+//Select all, add a burger, and update boolean. 
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+    //console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+    //console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+// Export the orm object for the model (burgers.js).
+module.exports = orm;
