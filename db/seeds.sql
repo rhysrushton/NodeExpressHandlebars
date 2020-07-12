42b8a2756581f5e764258da25441205ca51d1e4b
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-13 08:32:40 +1000

    seeds done.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-  
+/* Seeds for database*/ 
 INSERT INTO burgers (name) VALUES ('Burger 1');
 INSERT INTO burgers (name) VALUES ('Burger 2');
 INSERT INTO burgers (name, devour) VALUES ('Burger 3', true);
