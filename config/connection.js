dd9a644027488c1db06c1a7f0b88fe1e51a0b58e
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-13 08:12:27 +1000

    connection.js file done. Need to change to jaws db when pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,12 @@
 //This is the code for connecting to the sql database. 
 var mysql = require ("mysql")
+//require the dotenv package to store environmental variables. 
 const env = require("dotenv").config();
-
+//Define password variable so we can use it for db connection. 
 let password = process.env.PASSWORD
 
+//SQL function with password variable. 
+//Need to use JawsDB when uploading to heroku
 var connection = mysql.createConnection({
 host: "localhost",
   port: 3306,
@@ -12,6 +15,7 @@ host: "localhost",
   database: "burger_db"
 })
 
+//Make the connenction
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -20,4 +24,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+//Export the connection for ORM use
 module.exports = connection;
